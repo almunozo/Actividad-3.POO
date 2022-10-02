cf6135b306fc83e0398d2196a09b4d509b29f1b2
almunozo <almunozo@unal.edu.co>
2022-10-01 20:34:21 -0500

    Ejercicio7

---
+++ Ejercicio7/src/Ejercicio7/Comparar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio7;
+
+
+public class Comparar {
+    public String comparar (double a,double b){
+	if (a >b){
+            return"A es mayor que B";
+        }else if (a==b){
+            return"B es igual a A";
+        }else{
+            return"B es mayor que A";
+        }
+    }
+    
+}
